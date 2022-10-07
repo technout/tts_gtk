8c85e5cff1979f863330085d366273f50311f2fa
technout <technonout@live.nl>
2022-10-07 22:08:04 +0200

    update

--- tts_gtk.py
+++ tts_gtk.py
@@ -117,7 +117,7 @@ class Tts_gtk():
 
         # within gtk mainloop call this function:
         GLib.timeout_add(500, self.update_ram_status) # every 500 ms
-        # GLib.timeout_add(550, self.update_speaker_label)
+        # GLib.timeout_add(570, self.update_speaker_label)
 
         window.show_all()
         deleteOldFiles(MyUIDs)
